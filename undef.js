87e6b258589db5936346031ec9aafaf0abe69fa0
pawan <thakunipawan@gmail.com>
2024-04-16 06:29:03 +0530

    lexical

--- undef.js
+++ undef.js
@@ -1,3 +1,7 @@
+
+//Undefined===> whenever we will create any var or func in js ,it will create a ecution context ,
+// and before excuting that var it will allocate a memory to that var ,
+// and it will show as undefined in memory ,we can assume that undefined is a place holder for var before excuting
 var a=10;
 
 var x=23;
