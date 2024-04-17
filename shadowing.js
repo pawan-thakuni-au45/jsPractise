6ac94732d85f7b0ce926d8dafb23dc7bc76322c8
pawan <thakunipawan@gmail.com>
2024-04-17 07:29:26 +0530

    shadowing

---
+++ shadowing.js
@@ -0,0 +1,22 @@
+
+// var a=100;
+// {
+//     var a=10;
+//     let b=20;
+//     const c=30;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(
+
+let b=100;
+{
+    var a=10;
+    let b=20;
+    const c=30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(b)
